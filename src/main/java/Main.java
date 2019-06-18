f8a8d6b6ddd0f54e41e5ae9e440f585d1e8edf73
Nemodev <nemodeveloper1@gmail.com>
2019-06-18 11:13:47 +0300

    - добавлен режим выбора файла по приоритету

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,10 +1,20 @@
 import java.io.File;
+import java.util.Arrays;
+import java.util.List;
 
 public class Main
 {
     public static void main(String[] args)
     {
-        new EmployeePassProcessor(args[0]).process();
+        new EmployeePassProcessor(getFileName(args)).process();
+    }
+
+    private static String getFileName(String[] args)
+    {
+        List<String> argList = Arrays.asList(args);
+        return argList.size() != 0
+                ? argList.get(0)
+                : getBasePath() + "access_zone.xls";
     }
 
     public static String getBasePath()
